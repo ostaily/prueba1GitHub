d0634d326a9b74a42d1ffaa93dc06c74b218af4d
ostaily <ostaballero@gmail.com>
2024-06-10 19:53:29 -0500

    crear login

--- clase-05-06/js/logica.js
+++ clase-05-06/js/logica.js
@@ -1,1 +1,23 @@
 console.log('logica conectada');
+
+const usuario = 'Admin'
+const clave = '1234'
+
+function pruebaBoton(){
+
+    let user = document.getElementById('user').value
+    let pw = document.getElementById('pw').value
+    localStorage.setItem("user", user)
+    localStorage.setItem("pw", pw)
+
+    if(usuario==user && clave==pw){
+        window.open('calculadora.html', "_self")
+        
+    }else{ 
+        console.log('error de credenciales');
+    }
+}
+
+function cargarMensaje(){
+    document.getElementById('bienvenida').innerHTML = `Bienvenido ${localStorage.getItem("user")}`;
+}
